d2ec4cb582f029dc08c8fa5acc6e89e375686414
SamuelDuartedeCordova <samuelduartedecordova@gmail.com>
2023-11-22 20:49:39 -0300

    pasta interfaces

--- src/app/Service/login.service.ts
+++ src/app/Service/login.service.ts
@@ -3,7 +3,29 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
+export class AuthService {
+  private readonly userKey = 'currentUser';
 
-  constructor() { }
+  login(username: string, password: string): void {
+    // Simulação de chamada de API para autenticação
+    if (username === 'user' && password === 'pass') {
+      const user = { username: username };
+      sessionStorage.setItem(this.userKey, JSON.stringify(user));
+    } else {
+      console.error('Login falhou');
+    }
+  }
+
+  logout(): void {
+    sessionStorage.removeItem(this.userKey);
+  }
+
+  getCurrentUser(): any {
+    const userJson = sessionStorage.getItem(this.userKey);
+    return userJson ? JSON.parse(userJson) : null;
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getCurrentUser();
+  }
 }
