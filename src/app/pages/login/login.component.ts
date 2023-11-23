95514fe9cb9483f0564f09e054fe019663fe2ee3
Samuel Cordova <samuelduartedecordova@gmail.com>
2023-11-23 19:51:15 -0300

    Login components ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LoginService } from ';
+import { LoginService } from '../../services/login.service';
 
 
 @Component({
