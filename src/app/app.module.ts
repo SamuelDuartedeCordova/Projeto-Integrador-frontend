a6b0a8bf521a0e5231b0832795492cb41f97667f
Samuel Cordova <samuelduartedecordova@gmail.com>
2023-11-25 18:05:23 -0300

    atual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
-import { LoginComponent } from "./pages/login/login.component";
+import { LoginComponent } from "./login.component";
+import { AppRoutingModule } from "./app.routes";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: []
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LoginService } from '../../services/login.service';
+import { LoginService } from './services/login.service';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['login.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
