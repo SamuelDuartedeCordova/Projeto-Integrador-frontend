d4803b1938b252eaddc813793788e10811847933
Samuel Cordova <samuelduartedecordova@gmail.com>
2023-11-25 18:37:22 -0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
-import { LoginComponent } from "./login.component";
+import { LoginComponent } from "./login.component"
 import { AppRoutingModule } from "./app.routes";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
+    FormsModule,
+    RouterModule,
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
