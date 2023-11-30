9f6af80c87faa32b0282ec110c77b772784dd3b9
Luiz Naspolini <luiznds62@gmail.com>
2023-11-30 22:56:32 +0100

    Alterações

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-test-app' title`, () => {
+  it(`should have the 'Projeto-Integrador-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-test-app');
+    expect(app.title).toEqual('Projeto-Integrador-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-test-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Projeto-Integrador-frontend');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
 export const routes: Routes = [
-
+  {
+    path: 'login',
+    component: LoginPageComponent
+  }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
