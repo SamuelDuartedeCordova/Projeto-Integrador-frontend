aa16b73fb2c580c933b5dc9adb1046acb1eb5620
pauloo <silvas.pr@gmail.com>
2023-11-29 22:43:46 -0300

    commit implementando formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path:'',
+      redirectTo: 'cadastro',
+      pathMatch:'full'
+    },
+    {
+      path: 'cadastro',
+      component: CadastroComponent
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule { }
+
--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+
+  private codigo = 1;
+  cadastros: CadastroComponent[] = []
+
+  @ViewChild("formulario") formulario : NgForm | undefined
+
+  addCadastro(formulario: NgForm) {
+
+    if (formulario.valid) {
+      let cadastro: CadastroComponent = formulario.value;
+
+      if (cadastro.codigo > 0) {
+        this.excluir(cadastro);
+        this.cadastros.push(cadastro);
+
+      } else {
+        this.cadastros.push({
+          ...formulario.value,
+          codigo: this.codigo++})
+      }
+
+      this.formulario?.resetForm();
+    }
+
+  }
+
+  editar(item: CadastroComponent) {
+    if (this.formulario) {
+      this.formulario.setValue(item);
+    }
+  }
+
+  excluir(item: CadastroComponent) {
+    this.cadastros = this.cadastros.filter(c => c.codigo != item.codigo);
+
+  }
 
 }
