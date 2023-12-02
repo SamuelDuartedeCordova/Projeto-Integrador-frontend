5fdb969f7aa2ca27164967102f3ab6ff6ac52a01
SamuelDuartedeCordova <samuelduartedecordova@gmail.com>
2023-12-01 22:00:58 -0300

    register atual

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InputSenhaComponent} from "../../shared/input-senha/input-senha.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormErrorComponent} from "../../shared/form-error/form-error.component";
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [
+    InputSenhaComponent,
+    ReactiveFormsModule,
+    FormErrorComponent,
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    sobrenome: new FormControl('', [Validators.required]),
+    email: new FormControl('',  [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required]),
+  });
+
+  senhaFormControl: FormControl = undefined as any;
+
+  ngOnInit() {
+    this.senhaFormControl = this.loginForm.get('senha') as FormControl;
+  }
+
+  criarConta(): void {
+    this.loginForm.markAllAsTouched();
+  }
 }
