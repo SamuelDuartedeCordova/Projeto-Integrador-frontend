94910d52d539c33748b302127e8c5c942ebd6969
pauloo <silvas.pr@gmail.com>
2023-12-02 23:25:07 -0300

    commit address

--- src/app/pages/address-page/address-page.component.ts
+++ src/app/pages/address-page/address-page.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormErrorComponent } from '../../shared/form-error/form-error.component';
+import { InputSenhaComponent } from '../../shared/input-senha/input-senha.component';
 
 @Component({
   selector: 'app-address-page',
   standalone: true,
-  imports: [],
+  imports: [
+    InputSenhaComponent,
+    ReactiveFormsModule,
+    FormErrorComponent,
+  ],
   templateUrl: './address-page.component.html',
   styleUrl: './address-page.component.scss'
 })
-export class AddressPageComponent {
+export class AddressPageComponent implements OnInit {
+cadastrar() {
+throw new Error('Method not implemented.');
+}
+  loginForm = new FormGroup({
+    email: new FormControl('',  [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required]),
+  });
 
+  senhaFormControl: FormControl = undefined as any;
+addressForm: any;
+
+  ngOnInit() {
+    this.senhaFormControl = this.loginForm.get('senha') as FormControl;
+  }
+
+  login(): void {
+    this.loginForm.markAllAsTouched();
+  }
 }
