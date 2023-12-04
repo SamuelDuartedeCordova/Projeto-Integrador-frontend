cfed3afb4ee5019aa73f15be6a3c118205c033f8
Luiz Naspolini <luiznds62@gmail.com>
2023-12-04 21:12:04 +0100

    Cria spinner

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -3,8 +3,12 @@ import {InputSenhaComponent} from "../../shared/components/input-senha/input-sen
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FormErrorComponent} from "../../shared/components/form-error/form-error.component";
 import {AppLogoComponent} from "../../shared/components/app-logo/app-logo.component";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {UsuarioService} from "../../shared/services/usuario.service";
+import {SpinnerIconComponent} from "../../shared/components/spinner-icon/spinner-icon.component";
+import {finalize} from "rxjs";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register-page',
@@ -15,22 +19,26 @@ import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
     FormErrorComponent,
     AppLogoComponent,
     NavbarComponent,
+    SpinnerIconComponent,
+    NgIf,
   ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent implements OnInit {
 
+  processando: boolean = false;
+
   registroForm = new FormGroup({
     nome: new FormControl('', [Validators.required]),
     sobrenome: new FormControl('', [Validators.required]),
-    email: new FormControl('',  [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     senha: new FormControl('', [Validators.required]),
   });
 
   senhaFormControl: FormControl = undefined as any;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private usuarioService: UsuarioService) {
   }
 
   ngOnInit() {
@@ -46,5 +54,22 @@ export class RegisterPageComponent implements OnInit {
   criarConta(): void {
     this.registroForm.markAllAsTouched();
 
+    if (this.registroForm.valid) {
+      this.processando = true;
+
+      this.usuarioService.cadastrar({
+        nome: this.registroForm.get('nome')?.value as string,
+        sobrenome: this.registroForm.get('sobrenome')?.value as string,
+        email: this.registroForm.get('email')?.value as string,
+        senha: this.registroForm.get('senha')?.value as string,
+      })
+        .pipe(
+          finalize(() => this.processando = false)
+        )
+        .subscribe(response => {
+        window.alert('Conta criada com sucesso');
+        this.router.navigate(['home']);
+      });
+    }
   }
 }
