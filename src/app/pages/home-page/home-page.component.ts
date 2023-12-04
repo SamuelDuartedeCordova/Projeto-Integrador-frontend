9c364efb64d13f06ff3f35692a15b598e9e81145
SamuelDuartedeCordova <samuelduartedecordova@gmail.com>
2023-12-04 11:47:44 -0300

    home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  standalone: true,
-  imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  searchTerm: string = ''; // Use ngModel ou [(ngModel)] no modelo para vincular a essa propriedade
 
+  buscar() {
+    // Agora você pode acessar o searchTerm diretamente
+    console.log('Buscando por:', this.searchTerm);
+    // Implemente sua lógica de pesquisa aqui
+  }
 }
