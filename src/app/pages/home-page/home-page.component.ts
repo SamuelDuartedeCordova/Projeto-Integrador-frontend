1920247346c34b4250f9a511de3e013e194f19cd
Luiz Naspolini <luiznds62@gmail.com>
2023-12-04 22:08:04 +0100

    Cria home

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {CategoryBarComponent} from "./components/category-bar/category-bar.component";
+import Produto from "../../shared/models/produto.model";
+import {NgForOf} from "@angular/common";
+import {ProdutoCardItemComponent} from "./components/produto-card-item/produto-card-item.component";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  standalone: true,
+  imports: [NavbarComponent, CategoryBarComponent, NgForOf, ProdutoCardItemComponent]
 })
 export class HomePageComponent {
   searchTerm: string = ''; // Use ngModel ou [(ngModel)] no modelo para vincular a essa propriedade
+  produtos: Produto[] = [
+    {
+      foto: '/assets/images/bola_1.png',
+      nome: 'Bola de Futebol de Campo Penalty Bravo',
+      nota: 1,
+      percentualDesconto: 30,
+      preco: 119.90,
+      precoDesconto: 83.93
+    },
+    {
+      foto: '/assets/images/cone_com_furo_1.png',
+      nome: '10 Cones com 5 Barreiras e Sinalizador',
+      nota: 5,
+      percentualDesconto: 20,
+      preco: 99.99,
+      precoDesconto: 79.99
+    },
+    {
+      foto: '/assets/images/rede_1.png',
+      nome: 'Rede de Futebol Campo Master Fio 4mm Nylon Sem Nó',
+      nota: 1,
+      percentualDesconto: 30,
+      preco: 459.90,
+      precoDesconto: 321.30
+    },
+  ];
 
   buscar() {
     // Agora você pode acessar o searchTerm diretamente
---
+++ src/app/shared/models/produto.model.ts
@@ -0,0 +1,8 @@
+export default interface Produto {
+  foto: string;
+  percentualDesconto: number;
+  nome: string;
+  nota: number;
+  preco: number;
+  precoDesconto: number;
+}
